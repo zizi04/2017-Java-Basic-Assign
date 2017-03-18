b70ebd74c9eac95d0b5ac1f6b3ec0aed3c0d8cc1
hskang7 <hskang7@gmail.com>
2017-03-18 16:07:46 +0900

    배열과 표준입력 연습
    //프로그래밍 하세요. 
    부분을 코딩해서 다음과 같은 결과가 나오도록 하세요.

---
+++ Average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Average { 
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print("입력할 최대 실수의 갯수를 입력하세요. >> ");    
+        Scanner input = new Scanner(System.in);  
+        int size = input.nextInt();    
+        System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
+        System.out.print("실수 " + size + "개를 입력하세요. >> ");    
+        
+        //프로그래밍 하세요.
+        
+        System.out.println("합: " + sum + ", 평균: " + sum/size);
+    }
+}
